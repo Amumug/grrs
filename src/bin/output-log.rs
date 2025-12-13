3daae46ebb3ba7d37ff4154bde8113a4cf622dd8
Amumug <fabriali4@gmail.com>
2025-12-13 19:47:27 +0100

    test

--- src/bin/output-log.rs
+++ src/bin/output-log.rs
@@ -1,7 +1,8 @@
-use log::{info, warn};
+use log::{info, warn, trace};
 
 fn main() {
     env_logger::init();
     info!("Starting up");
     warn!("oops, nothing implemented!");
+    trace!("trace testing!");
 }
