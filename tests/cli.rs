d33aeb79ee9d0edfb25d5d1ecfffbd989574833d
Amumug <fabriali4@gmail.com>
2025-12-28 23:37:19 +0100

    test

--- tests/cli.rs
+++ tests/cli.rs
@@ -12,3 +12,17 @@ fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
 
     Ok(())
 }
+
+#[test]
+fn find_content_in_file() -> Result<(), Box<dyn std::error::Error>> {
+    let file = assert_fs::NamedTempFile::new("sample.txt")?;
+    file.write_str("A test\nActual content\nMore content\nAnother test")?;
+
+    let mut cmd = cargo_bin_cmd!("grrs");
+    cmd.arg("test").arg(file.path());
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains("A test\nAnother test"));
+
+    Ok(())
+}
